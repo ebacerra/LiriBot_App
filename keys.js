24fd2fbde55ce2bb0c9b523d428830de9ad01db8
Ellen <ellen.bacerra@gmail.com>
2018-08-28 21:58:15 -0500

    update all dir

--- keys.js
+++ keys.js
@@ -1,15 +1,11 @@
 console.log('this is loaded');
 
-// exports.spotify = {
-//     id: process.env.SPOTIFY_ID,
-//     secret: process.env.SPOTIFY_SECRET
-// };
-
-
 
 exports.spotify = {
-    SPOTIFY_ID: '67382ac864cf4dd8927fac14f8d31918',
-    secret: '7c5d4aba1fbe441b9ffad319288c2147'
+    api_id: process.env.api_id
+    secret_id: process.env.secret_id
 };
 
-var spotify = new Spotify(keys.spotify);
+exports.omdb = {
+    api_key: process.env.API_KEY
+}
--- liri.js
+++ liri.js
@@ -7,26 +7,34 @@ var spotify = require("node-spotify-api");
 var keys = ("keys.js")
 
 var userInput = process.argv[2];
+var nodeArgs = process.argv;
+
+var spotify = new Spotify(keys.spotify);
+
 
 if (userInput === "movie-this") {
     console.log("I like this movie")
+    myMovie();
 
 
 } else
     if (userInput === "spotify-this-song") {
         console.log("spotify song")
+        mySpotify();
 
 
 
     } else
         if (userInput === "concert-this") {
             console.log("this concert is awesome")
+            myBand();
+
         }
 
         else
             if (userInput === "do-what-it-says") {
                 console.log("say it")
-
+                myRandomtxt();
             }
 
 
@@ -35,7 +43,7 @@ if (userInput === "movie-this") {
 
 function myMovie() {
     // this will store arguments in an array
-    var nodeArgs = process.argv;
+
 
     // create an empty variable to hold movie name
     var movieName = "";
@@ -66,30 +74,115 @@ function myMovie() {
         if (!error && response.statusCode === 200) {
             var body = JSON.parse(body);
 
-            // need to console,log:
-            //     * Title of the movie.
-            //    * Year the movie came out.
-            //    * IMDB Rating of the movie.
-            //    * Rotten Tomatoes Rating of the movie.
-            //    * Country where the movie was produced.
-            //    * Language of the movie.
-            //    * Plot of the movie.
-            //    * Actors in the movie.
+            console.log(body)
+            // ^^do I still need this?
+
+            console.log(["\n Movie Title: " + body.Title + "\n", "\n Year:  " + body.Year + "\n", "\n IMDB Rating: " + body.Rating + "\n", "\n Rotten Tomatoes Rating: " + body.Rating + "\n", "\n Country: " + body.Country + "\n", "\n Movie Language: " + body.Language + "\n", "\n Plot: " + body.Plot + "\n", "\n Actors: " + body.Actor + "\n"]);
 
 
         } else {
             console.log(error);
         }
 
+    });
+};
+
+
+// my spotify API
+function mySpotify() {
+    spotify.search({ type: 'track', query: 'Perfect' }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+
+        } else {
+            // not sure if I'm doing this correctly - googled this one(stackoverflow) and from our activity
+            console.log("\nArtist: " + JSON.stringify(data.tracks.items[0], artist[0].name, null, 2) + "\n ");
+            console.log("\nSong Title: " + JSON.stringify(data.tracks.items[0].name + "\n ");
+            console.log("\nAlbum: " + JSON.stringify(data.tracks.items[0].album.name + "\n ");
+            console.log("\nLink: " + JSON.stringify(data.tracks.items[0].album.preview_url + "\n ");
+        }
+
+        console.log(data);
+    });// ^^Do I still need this?
+};
+
+
+
+//   Ramdom:
+function myRandomtxt() {
+    fs.readFile(`random.txt`, "utf8", function (error, data) {
+
+        if (error) {
+            return console.log(error);
+        }
+
+
+        console.log(data);
+
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+
+        // We will then re-display the content as an array for later use.
+        console.log(dataArr);
 
     });
-
-
-
-
-
-
 };
+
+
+// myBand
+function myBand() {
+    // this will store arguments in an array
+
+    var dates = moment().format('MM Do YYYY');
+    console.log(dates);
+    // create an empty variable to hold movie name
+    var bandName = "";
+
+    for (var i = 2; i < nodeArgs.length; i++) {
+
+        if (i > 2 && i < nodeArgs.length) {
+
+            bandName = bandName + "+" + nodeArgs[i];
+
+        }
+
+        else {
+
+            bandName += nodeArgs[i];
+
+        }
+    }
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + bandName + "&y=&plot=short&apikey=trilogy";
+
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
+
+    request(queryUrl, function (error, response, body) {
+
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+            var body = JSON.parse(body);
+
+            console.log(body)
+            // ^^do I still need this?
+
+            console.log(["\n Venue Name: " + body.Venue + "\n", "\n Location:  " + body.Location + "\n", "\n Event Dates: " + body.Dates = dates + "\n"]);
+            //****   not sure if I pass this correctly for Moments***
+
+            // * Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+        } else {
+            console.log(error);
+        }
+
+    });
+};
+
+
+
+
+
 
 
 
